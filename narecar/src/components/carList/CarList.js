4aea1a113d2fcb7b66a0d9b309f8a164b6425292
nareesuarezz <naremehs11@gmail.com>
2023-04-18 16:30:03 +0100

    FIX search bar

--- narecar/src/components/carList/CarList.js
+++ narecar/src/components/carList/CarList.js
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import CarService from "../../services/car.services";
 import "./CarList.css"
 
 function CarList() {
+  const [cars, setCars] = useState([]);
+  const [query, setQuery] = useState("");
+
   const getCars = () => {
     const allCars = CarService.getCars();
+    setCars(allCars);
+  }
 
+
+  const showCars = () => {
     return (
-      allCars.map(c => {
+      cars.filter(c => c.brand.toLowerCase().includes(query.toLowerCase()) ||
+                       c.model.toLowerCase().includes(query.toLowerCase()) ||
+                       c.price.toString().toLowerCase().includes(query.toLowerCase())).map(c => {
         return (
           <div className="car-list-item">
             <div className="car-list-img">
@@ -19,11 +29,22 @@ function CarList() {
     );
   }
 
+  const handleChange = (event) => {
+    const searchValue = event.target.value;
+    setQuery(searchValue);
+  }
+
+  useEffect(() => {
+    getCars();
+  }, []);
+
   return (
     <>
+      <input type="search" onChange={handleChange} />
+      {/* <input type="search" onChange={handleChange} /> */}
       <div className="main-container">
         <div className="car-list-container">
-          {getCars()}
+          {showCars()}
         </div>
         <div className="right-text">
           <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
